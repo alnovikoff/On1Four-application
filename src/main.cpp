0eba042691c1f22640d34c37f612533ba9d28cd4
Alexandr Novikov <93482331+alnovikoff@users.noreply.github.com>
2023-07-27 16:13:23 +0500

    pch was added

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <engine.h>
 #include <glm/glm.hpp>
-#include <service_locator.h>
 
 int main(int argc, char **argv)
 {
   std::cout << "HEllo from sandbox" << std::endl;
-  On1FourEngine::Init();
+  print_int(5);
+  // On1FourEngine::Init();
 
-  ServiceLocator::GetWindow()->OpenWindow();
+  // ServiceLocator::GetWindow()->OpenWindow();
 
-  while(!ServiceLocator::GetWindow()->Update())
-  {
+  // while(!ServiceLocator::GetWindow()->Update())
+  // {
 
-  }
+  // }
 
   return 0;
 }
